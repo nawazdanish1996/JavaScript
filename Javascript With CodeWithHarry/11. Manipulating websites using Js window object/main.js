0ef5dd23b0adae2219403edd622e44838d6ea1d7
Nawaz Danish <nawazdanish1996@gmail.com>
2022-07-25 03:07:07 +0530

    initial commit

---
+++ Javascript With CodeWithHarry/11. Manipulating websites using Js window object/main.js
@@ -0,0 +1,17 @@
+let a = window.document;
+a = innerHeight;
+a = innerWidth;
+a = scrollX;
+a = scrollY;
+a = location;
+a = location.toString();
+a = history;
+// a = history.go();
+
+// alert(`Hello Nawaz Danish`);
+
+// a = prompt(`This will destroy your computer, Type your name`);
+
+// a = confirm(`Are you sure do you want to delete`)
+
+console.log(a);
