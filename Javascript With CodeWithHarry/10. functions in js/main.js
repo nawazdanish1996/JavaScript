956d799959702ff56f523c57ae596deafb0c19ac
Nawaz Danish <nawazdanish1996@gmail.com>
2022-07-25 01:13:21 +0530

    initial commit

--- Javascript With CodeWithHarry/10. functions in js/main.js
+++ Javascript With CodeWithHarry/10. functions in js/main.js
@@ -1,1 +1,41 @@
-console.log("Welcome");
+function greet(name, thank){
+    console.log(`Happy Birthday ${name} 
+    How I wish I could fly to you right now and be with you on this special day of yours but remember, my good wishes are always there with you.
+    Happy Birthday! ${thank} :)`);
+
+}
+
+let name = "SkillF";
+let name2 = "Nawaz";
+// greet(name, 'Thanks a lot');
+
+
+const obj = {
+    name: "SkillF",
+    game: function(){
+        return "GTA"
+    }
+}
+// console.log(obj.game());
+
+
+arr = ['fruit', 'vegetable', 'furniture'];
+arr.forEach(function(element, index, array) {
+    // console.log(element, index);
+});
+
+
+let i = 234;
+// console.log(i);
+function ui(name){
+    let i = 9;
+    // i = 9;
+    console.log(i);
+    return `This is a ${name} ui`;
+}
+console.log(ui("Nawaz Danish"), i);
+
+/* Notes
+let / const has block level scope!
+var has function level scope!
+*/
